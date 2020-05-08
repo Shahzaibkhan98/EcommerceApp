e3a1448cd44deb342e2d5f9d8429e7d866a49555
Shahzaibkhan98 <45264436+Shahzaibkhan98@users.noreply.github.com>
2020-05-09 00:16:56 +0500

    First Commit

---
+++ app/src/main/java/com/example/ecommerce/Model/AdminOrders.java
@@ -0,0 +1,85 @@
+package com.example.ecommerce.Model;
+
+public class AdminOrders {
+
+    private String address, phone, name, date, time, city, state, totalAmount;
+
+    public AdminOrders() {
+
+    }
+
+    public AdminOrders(String address, String phone, String name, String date, String time, String city, String state, String totalAmount) {
+        this.address = address;
+        this.phone = phone;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.city = city;
+        this.state = state;
+        this.totalAmount = totalAmount;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
